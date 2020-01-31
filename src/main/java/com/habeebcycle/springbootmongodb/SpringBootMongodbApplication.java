322659abe668541cd8d4bc02899a54d77121191e
Habeeb Okunade <guessy2k2@yahoo.com>
2020-01-31 16:24:30 +1100

    'connect-atlas'

--- src/main/java/com/habeebcycle/springbootmongodb/SpringBootMongodbApplication.java
+++ src/main/java/com/habeebcycle/springbootmongodb/SpringBootMongodbApplication.java
@@ -1,9 +1,13 @@
 package com.habeebcycle.springbootmongodb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SpringBootMongodbApplication {
 
     public static void main(String[] args) {
